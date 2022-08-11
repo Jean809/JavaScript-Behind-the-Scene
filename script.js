60e6a221d8807ffd7b569d83689619ffaaf88736
Jean809 <jean_carlosrosario709@outlook.com>
2022-08-10 21:52:07 -0400

    re-assigned a variable inside a block function to see what happens to the template literal

--- script.js
+++ script.js
@@ -9,10 +9,20 @@ function calcAge(birthYear) {
 
     if (birthYear >= 1981 && birthYear <= 1996) {
       var millenial = true;
+      //What happens if we declare a variable that already exists?
+      const firstName =
+        "Nub"; /*Javascript always tries to look for the current "scope" to see if the variable is available. And since it is available, then it doesn't "look up" for other scopes to find that variable*/
       const str = `Oh, and you're a millenial, ${firstName}.`;
       console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
     }
+    console.log(millenial);
+    //  console.log(add(5, 6)); ----> this will give an error as it is being called outside of scope
   }
+
   printAge();
   return age;
 }
